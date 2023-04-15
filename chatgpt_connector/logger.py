cd374ba54e049b2511b3edf5f4666893dac8959e
yujidn <yu.ji.d.is+github2@gmail.com>
2023-04-15 15:20:13 +0900

    loggerを追加

---
+++ chatgpt_connector/logger.py
@@ -0,0 +1,24 @@
+import functools
+import json
+import logging
+import os
+from datetime import datetime
+
+
+@functools.lru_cache()
+def get_logger() -> logging.Logger:
+    with open(os.path.join(os.path.dirname(__file__), "logger.json")) as f:
+        log_conf = json.load(f)
+
+    # ファイル名をタイムスタンプで作成
+    log_conf["handlers"]["fileHandler"]["filename"] = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "logs",
+        "{}.logs".format(datetime.utcnow().strftime("%Y%m%d%H%M%S")),
+    )
+
+    logging.config.dictConfig(log_conf)
+
+    logger = logging.getLogger("gpt_logger")
+    return logger
